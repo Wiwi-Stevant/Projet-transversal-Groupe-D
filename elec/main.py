3c22d0cd6f022556bdd7605774d88aa28402f8f2
Wiwi-Stevant <he202476@students.ephec.be>
2026-05-12 16:25:33 +0200

    mqtt backend

--- elec/main.py
+++ elec/main.py
@@ -3,105 +3,66 @@ from umqtt.simple import MQTTClient
 import network
 import utime
 
-# =========================
-# CONFIGURATION MQTT
-# =========================
-
-MQTT_BROKER = "broker.hivemq.com"
-
-# Le CLIENT_ID doit être unique
-CLIENT_ID = "PicoW_Compteur_Click_123456"
-
-# Topic MQTT
-TOPIC_CLICK = b"pico/compteur/click"
-
-# Variables globales
+# Variable
 global wlan, client, led, bouton, compteur
 
+    # WiFi
 SSID = "iPhone de William"
-PWD = "..."
+PWD = "partage11St"
 
-# =========================
-# CONNEXION WIFI
-# =========================
+    # MQTT
+MQTT_BROKER = "172.20.10.13"
+CLIENT_ID = "PicoW_Compteur_Click_123456"
+TOPIC_CLICK = b"sensors/data"
+TOPIC_LED = b"sensors/led"
 
+# connection wifi
 def connect_wifi():
     global wlan
-
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(SSID, PWD)
-
     print("Connexion au WiFi...")
-
     while not wlan.isconnected():
         print("En attente de connexion...")
         utime.sleep(1)
-
     print("Connecté avec l'IP :", wlan.ifconfig()[0])
 
-
-# =========================
-# CONNEXION MQTT
-# =========================
-
+# connection MQTT
 def connect_mqtt():
     global client
-
     client = MQTTClient(CLIENT_ID, MQTT_BROKER)
     client.connect()
-
     print("Connecté au Broker MQTT")
-
-
-# =========================
-# INITIALISATION
-# =========================
 
 def init():
     global led, bouton, compteur
-
-    # WiFi + MQTT
     connect_wifi()
     connect_mqtt()
 
-    # GPIO
     led = Pin(2, Pin.OUT)
     bouton = Pin(0, Pin.IN, Pin.PULL_UP)
-
     compteur = 0
-
     print("Système prêt")
 
-
-# =========================
-# ENVOI MQTT
-# =========================
-
+# envoie vers le broquer
 def send_click():
     global compteur, client
-
     compteur += 1
-
-    # Heure actuelle
     temps = utime.localtime()
-
     date_heure = "{:02d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}".format(
         temps[2], temps[1], temps[0],
         temps[3], temps[4], temps[5]
     )
 
-    # Message à envoyer
-    message = '{{"count": {}, "time": "{}"}}'.format(
-        compteur,
-        date_heure
-    )
-
+# console
     print("----------------")
     print("Bouton appuyé")
     print("Compteur :", compteur)
     print("Heure :", date_heure)
 
+    # envoye
+    message = '{{"type": "entree", "value": {}, "device_id": "pico_w_001"}}'.format(compteur)
     try:
         client.publish(TOPIC_CLICK, message)
         print("Donnée publiée sur MQTT :", message)
@@ -116,38 +77,43 @@ def send_click():
         except:
             print("Échec de reconnexion MQTT")
 
+def mqtt_callback(topic, msg):
+    global led
 
-# =========================
-# BOUCLE PRINCIPALE
-# =========================
+    message = msg.decode().strip().lower()
+
+    print("Message reçu sur", topic.decode(), ":", message)
+
+    if topic == TOPIC_LED:
+
+        if message == "on":
+            led.value(1)
+            print("LED allumée à distance")
+
+        elif message == "off":
+            led.value(0)
+            print("LED éteinte à distance")
+
+        else:
+            print("Commande inconnue : utiliser 'on' ou 'off'")
 
 def mainloop():
     global bouton, led
-
     while True:
-
-        # Si bouton appuyé
+        #client.check_msg()
+        
         if bouton.value() == 0:
-
             led.value(1)
-
             send_click()
 
             # Anti-rebond
             utime.sleep(0.3)
 
-            # Attendre relâchement
             while bouton.value() == 0:
                 pass
 
             led.value(0)
-
         utime.sleep(0.01)
-
-
-# =========================
-# LANCEMENT
-# =========================
 
 init()
 mainloop()
