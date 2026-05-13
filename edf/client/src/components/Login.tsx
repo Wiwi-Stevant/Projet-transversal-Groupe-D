332649eb712c9e507ee905a26aa870162886d661
Fscalais <he202124@students.ephec.be>
2026-05-13 14:07:19 +0200

    Merge branch 'CI/CD' into dev

--- edf/client/src/components/Login.tsx
+++ edf/client/src/components/Login.tsx
@@ -16,11 +16,11 @@ export default function Login({ onLoginSuccess }: LoginProps) {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
       const data = await response.json();
 
